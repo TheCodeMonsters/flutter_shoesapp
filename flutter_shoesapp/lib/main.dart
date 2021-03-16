0ac767f4212245b2e85f3e344097a1bed6a0504e
Manuel <carlosd99m@gmail.com>
2021-03-16 03:01:54 -0600

    Provider de seleccion de talla

--- flutter_shoesapp/lib/main.dart
+++ flutter_shoesapp/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shoesapp/src/models/zapato_model.dart';
 import 'package:flutter_shoesapp/src/pages/zapato_page.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  return runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => new ZapatoModel() ),
+      ],
+
+      child: MyApp(),
+    )
+    );
+}
 
 class MyApp extends StatelessWidget {
   @override
