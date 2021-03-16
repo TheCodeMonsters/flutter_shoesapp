b094a0adafe3056733eed1ac4a171305cf078644
Manuel <carlosd99m@gmail.com>
2021-03-15 21:13:47 -0600

    descripción del zapato

--- flutter_shoesapp/lib/src/pages/zapato_page.dart
+++ flutter_shoesapp/lib/src/pages/zapato_page.dart
@@ -9,7 +9,21 @@ class ZapatoPage extends StatelessWidget {
         children: <Widget>[
           CustomAppBar('For You'),
           SizedBox(height: 20),
-          ZapatoSizePreview(),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  ZapatoSizePreview(),
+                  ZapatoDescripcion(
+                    titulo: 'Nike Air Max 720',
+                    descripcion:
+                        "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
